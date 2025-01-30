cab8f3b17962c3ebd45305e616c0a442e8107628
unknown <carlosmviegas86@gmail.com>
2025-01-30 20:04:47 +0000

    added cors as a middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 
 const PORT = 8000;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT} and listening to requests...`));
