7a973eda785350b871890c5c64681d86246102ba
unknown <carlosmviegas86@gmail.com>
2025-01-30 19:58:40 +0000

    setup express server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+const express = require('express');
 
+const PORT = 8000;
+
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`server connected on port ${PORT} and listening to requests...`));
