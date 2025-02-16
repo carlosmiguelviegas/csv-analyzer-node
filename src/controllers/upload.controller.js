11e29d60283d5689cbf63bd7d3591cb50493b694
unknown <carlosmviegas86@gmail.com>
2025-02-16 18:00:54 +0000

    created an upload controller

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,15 +1,7 @@
-const signin = async(req, res) => {
-  
-  const { email, password } = req['body'];
-
-  if (!email || !password) {
-    return res.status(500).json({ message: `Incorrect email or password.` });
-  }
-
-  return res.status(200);
+const analyze = async(req, res) => {
 
 };
 
 module.exports = {
-  signin
+  analyze
 };
--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const parser = require('csv-parser');
-const multer = require('multer');
 
 const PORT = 8000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-
-const upload = multer({ dest: 'files' });
-
-app.post('/analyze', upload.single('file'), (req, res) => {
-
-  const { path } = req['file'];
-  const rows = [];
-  
-  fs.createReadStream(path)
-  .pipe(parser())
-  .on('data', chunck => rows.push(chunck))
-  .on('end', () => {
-    fs.unlink(path, (err) => { if (err) console.log(err) });
-    res.status(200).json({ message: `File processed with success. It were processed ${rows.length} records.` });
-  })
-  .on('error', (err) => {
-    if (err) res.status(500).json({ message: `An error occurred while processing the file.` });
-  });
-});
-
-app.post('/signingin', (req, res) => {
-
-  const { email, password } = req['body'];
-  
-});
 
 app.listen(PORT, () => console.log(`server connected on port ${PORT} and listening to requests...`));
