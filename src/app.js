b21b2e3591bcfc47b4ef56ba6bed329a37f3c989
unknown <carlosmviegas86@gmail.com>
2025-02-16 17:35:16 +0000

    created api router and imported in app module

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const cors = require('cors');
-const helmet = require('helmet');
-const compression = require('compression');
 
 const apiRouter = require('./routes/api');
 
 const app = express();
 
-app.use(helmet());
 app.use(cors());
-app.use(compression());
 app.use(express.json());
 
 app.use('/api/v1', apiRouter);
---
+++ src/routes/api.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const apiRouter = express.Router();
+
+/* apiRouter.use('/users', usersRouter);
+apiRouter.use('/books', booksRouter);
+apiRouter.use(errorHandler); */
+
+module.exports = apiRouter;
