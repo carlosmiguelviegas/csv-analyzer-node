d9ecd9fa691f786ceb3b2d29a8d34e08751d5110
unknown <carlosmviegas86@gmail.com>
2025-02-16 18:02:46 +0000

    moved the analyze method to the upload controller

--- src/controllers/upload.controller.js
+++ src/controllers/upload.controller.js
@@ -1,4 +1,18 @@
 const analyze = async(req, res) => {
+  
+  const { path } = req['file'];
+  const rows = [];
+    
+  fs.createReadStream(path)
+  .pipe(parser())
+  .on('data', chunck => rows.push(chunck))
+  .on('end', () => {
+    fs.unlink(path, (err) => { if (err) console.log(err) });
+    res.status(200).json({ message: `File processed with success. It were processed ${rows.length} records.` });
+  })
+  .on('error', (err) => {
+    if (err) res.status(500).json({ message: `An error occurred while processing the file.` });
+  });
 
 };
 
