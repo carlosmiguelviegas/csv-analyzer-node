18654a506c8d4408d0363d8e05220165bcef44ae
unknown <carlosmviegas86@gmail.com>
2025-02-16 18:05:42 +0000

    imported the analyze method to the analyze route

--- src/routes/upload.route.js
+++ src/routes/upload.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
+const { analyze } = require('../controllers/upload.controller');
+
 const upload = multer({ dest: 'files' });
 
 const uploadRouter = express.Router();
 
-usersRouter.post('/analyze', upload.single('file'));
+usersRouter.post('/analyze', upload.single('file'), analyze);
 
 module.exports = uploadRouter;
