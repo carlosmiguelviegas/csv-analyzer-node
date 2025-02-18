079e1ed6b62017aa7c206bb3f7fa6afcd4b8180e
unknown <carlosmviegas86@gmail.com>
2025-02-18 18:03:26 +0000

    send a message when the user has successfully signed in

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,7 +6,7 @@ const signin = async(req, res) => {
     return res.status(500).json({ message: `Incorrect email or password.` });
   }
 
-  return res.status(200);
+  return res.status(200).json({ message: `Successfully signed in.` });
 
 };
 
